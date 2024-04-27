0765778d98cf6cd0ce48b3ef02d0ca7bc36847b8
kabita164 <kabita164@gmail.com>
2024-04-28 00:10:16 +0100

    path correction for gh page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import backgroundImg from "/assets/Walking-Outside.svg";
+import entriesImg from "/assets/entries.svg";
+import trendsImg from "/assets/trends.svg";
 
 const Home = () => {
   return (
@@ -16,26 +19,18 @@ const Home = () => {
         </div>
         <div className="home-grid">
           <Link className="home-grid-col" to="/entries">
-            <img
-              src="/src/assets/entries.svg"
-              alt=""
-              className="home-grid-col-img"
-            />
+            <img src={entriesImg} alt="" className="home-grid-col-img" />
             <h3>View all entries</h3>
           </Link>
           <Link className="home-grid-col" to="/analysis">
-            <img
-              src="/src/assets/trends.svg"
-              alt=""
-              className="home-grid-col-img"
-            />
+            <img src={trendsImg} alt="" className="home-grid-col-img" />
             <h3>View mood trends</h3>
           </Link>
         </div>
       </div>
 
       <div className="background-image-wrapper">
-        <img src="/src/assets/Walking-Outside.svg" alt="" />
+        <img src={backgroundImg} alt="" />
       </div>
     </>
   );
